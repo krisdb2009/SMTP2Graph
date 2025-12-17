e828a1e107a0a3e83f796276f8bc706f80f46854
krisdb2009 <dylan@belowaverage.org>
2025-12-16 23:02:33 -0500

    .

--- SMTP2Graph/Program.cs
+++ SMTP2Graph/Program.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Graph;
 using Azure.Identity;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.RegularExpressions;
 using Microsoft.Graph.Models;
+using System.Text;
 
+Console.WriteLine("Initializing...");
+var mail_listener = new TcpListener(IPAddress.Any, 25);
 var scopes = new[] { "https://graph.microsoft.com/.default" };
-
 var options = new ClientSecretCredentialOptions
 {
     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
 };
-
 var clientSecretCredential = new ClientSecretCredential(
     Environment.GetEnvironmentVariable("TENANT_ID"),
     Environment.GetEnvironmentVariable("CLIENT_ID"),
@@ -16,17 +20,149 @@ var clientSecretCredential = new ClientSecretCredential(
     options
 );
 
-
+Console.WriteLine("Setting up MS Graph Service Client...");
 var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
 
-await graphClient.Users["support@belowaverage.org"].SendMail.PostAsync(new()
+Console.WriteLine("Starting SMTP listener loop...");
+mail_listener.Start();
+while (true)
 {
-    Message = new()
+    try {
+        var client = mail_listener.AcceptTcpClient();
+        Console.WriteLine("Accepting connection, attempting to process...");
+        _ = Task.Run(() => {
+            client.NoDelay = true;
+            var stream = client.GetStream();
+            var sw = new StreamWriter(stream);
+            var sr = new StreamReader(stream);
+            sw.AutoFlush = true;
+            sw.WriteLine($"220 Who is it...?");
+            HandleConnection(client, stream, sw, sr);
+        });
+    }
+    catch (Exception e)
     {
-        From = new() { EmailAddress = new() { Address = "support@belowaverage.org" } },
-        ToRecipients = [new() { EmailAddress = new() { Address = "dylan@belowaverage.org" } }],
-        Subject = "Hi",
-        Body = new() { ContentType = BodyType.Html, Content = "Hi" }
-    },
-    SaveToSentItems = false
-});
+        Console.WriteLine("An error occured in the SMTP listener loop:");
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+    }
+}
+
+void HandleConnection(TcpClient client, Stream stream, StreamWriter sw, StreamReader sr)
+{
+    string From = string.Empty;
+    List<string> To = new();
+    string Body = string.Empty;
+    string Subject = string.Empty;
+    BodyType BType = BodyType.Text;
+    while (true)
+    {
+        var msg = sr.ReadLine();
+        if (msg.StartsWith("HELO"))
+        {
+            sw.WriteLine("250 Whatchu want?");
+            continue;
+        }
+        if (msg.StartsWith("MAIL FROM:"))
+        {
+            From = Regex.Match(msg, "<(.*)>").Groups[1].Value;
+            sw.WriteLine("250 You again?!");
+            continue;
+        }
+        if (msg.StartsWith("RCPT TO:"))
+        {
+            To.Add(Regex.Match(msg, "<(.*)>").Groups[1].Value);
+            sw.WriteLine("250 Ok fine...");
+            continue;
+        }
+        if (msg.StartsWith("DATA"))
+        {
+            sw.WriteLine("354 And whats so important that you have to bother me...?");
+            (BType, Subject, Body) = ReadData(sr);
+            sw.WriteLine("250 Oh yea, that was *very* important... SMH.");
+            continue;
+        }
+        if (msg.StartsWith("QUIT"))
+        {
+            sw.WriteLine("221 Bye, don't talk to me...");
+            break;
+        }
+    }
+    client.Close();
+    Console.WriteLine("Sending to MS Graph...");
+    SendMessage(From, To, Subject, Body, BType);
+    Console.WriteLine("Done.");
+}
+
+(BodyType, string, string) ReadData(StreamReader sr)
+{
+    string Subject = string.Empty;
+    string Body;
+    BodyType BType = BodyType.Text;
+    bool Base64 = false;
+    while (true)
+    {
+        var msg = sr.ReadLine();
+        if (msg.StartsWith("Subject:"))
+        {
+            Subject = Regex.Match(msg, "Subject: (.*)").Groups[1].Value;
+            continue;
+        }
+        if (msg.StartsWith("Content-Type:"))
+        {
+            if (msg.Contains("html")) BType = BodyType.Html;
+            continue;
+        }
+        if (msg == "Content-Transfer-Encoding: base64")
+        {
+            Base64 = true;
+            continue;
+        }
+        if (msg == string.Empty) 
+        {
+            Body = ReadBody(sr, Base64);
+            break;
+        }
+    }
+    return (BType, Subject, Body);
+}
+
+string ReadBody(StreamReader sr, bool Base64)
+{
+    string Body = string.Empty;
+    while (true)
+    {
+        var msg = sr.ReadLine();
+        if (msg == ".") break;
+        if (msg == "..")
+        {
+            msg = ".";
+        }
+        Body += msg;
+    }
+    if (Base64)
+    {
+        Body = Encoding.UTF8.GetString(Convert.FromBase64String(Body));
+    }
+    return Body;
+}
+
+void SendMessage(string From, List<string> To, string Subject, string Body, BodyType BType)
+{
+    List<Recipient> recipients = new();
+    foreach (string email in To)
+    {
+        recipients.Add(new() { EmailAddress = new() { Address = email } });
+    }
+    _ = graphClient.Users[From].SendMail.PostAsync(new()
+    {
+        Message = new()
+        {
+            From = new() { EmailAddress = new() { Address = From } },
+            ToRecipients = recipients,
+            Subject = Subject,
+            Body = new() { ContentType = BType, Content = Body }
+        },
+        SaveToSentItems = false
+    });
+}
